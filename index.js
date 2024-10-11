673217ff81f28d8d2ab84288e58b967501823e3e
MandlakheM <142303272+MandlakheM@users.noreply.github.com>
2024-10-11 10:09:46 +0200

    server created

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log('hello i am Xperere')
+import express from "express";
 
+const app = express()
+const PORT = 8080
 
+app.get("/", (req, res) => res.send("hello from back end"));
+
+app.listen(PORT, () =>
+    console.log(`server listening on http://localhost:${PORT}`)
+  );
